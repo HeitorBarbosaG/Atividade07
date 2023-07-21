8a3b456a74c212d421113e63c4dbaecb01d6c355
HeitorBarbosaG <132144330+HeitorBarbosaG@users.noreply.github.com>
2023-07-20 21:05:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let idade = '0'
+const ano = '2023'
+let ano_do_user = prompt("Digite o ano em que nasceu: ")
+let nome_do_user = prompt('Digite seu nome: ')
+idade = ano-ano_do_user
+document.write('Olá, seu nome é ')
+document.write(nome_do_user)
+document.write(' e você tem ')
+document.write(idade)
+document.write(' anos.')
